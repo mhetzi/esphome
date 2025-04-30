f970e2344377fbf6064505e15d378f5a3b15fb71
Marcel Hetzendorfer <mh7596@gmail.com>
2025-04-30 12:57:41 +0200

    Soft Reset Device on error

--- esphome/components/vl53l0x/vl53l0x_sensor.cpp
+++ esphome/components/vl53l0x/vl53l0x_sensor.cpp
@@ -260,14 +260,26 @@ void VL53L0XSensor::setup() {
 
 void VL53L0XSensor::update() {
   if (this->initiated_read_ || this->waiting_for_interrupt_) {
-    this->publish_state(NAN);
-    this->status_momentary_warning("update", 5000);
     ESP_LOGW(TAG, "%s - update called before prior reading complete - initiated:%d waiting_for_interrupt:%d",
              this->name_.c_str(), this->initiated_read_, this->waiting_for_interrupt_);
     reg(0x0B) = 0x01;
     reg(0x0B) = 0x00;
     this->waiting_for_interrupt_ = false;
     this->initiated_read_ = false;
+    this->update_skipps++;
+    if (this->update_skipps > 10) {
+      this->update_skipps = 0;
+      this->publish_state(NAN);
+      this->status_momentary_warning("update", 5000);
+      reg(0xbf) = 0x00;
+      delay(100);
+      reg(0xbf) = 0x01;
+      while(reg(0xc0) == 0x00){
+        ESP_LOGD(TAG, "Device not yet ready")
+        delay(500);
+      }
+      this->setup();
+    }
     return;
   }
 
